d880ae65a579cd9070a56eb9345974b40c20be76
Michael Ishimoto <michaelish@yahoo.com>
2015-03-26 16:58:24 -0700

    Added another page (unused)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "pages#login"
+  get "traineeFinance" => "pages#traineeFinance"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
